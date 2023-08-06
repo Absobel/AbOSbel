30ad9b386bcb7726f375ea0f3d7f5dccbe55e7db
Absobel <imkonnu@gmail.com>
2023-08-06 14:13:18 +0900

    kvm

--- src/real_main.rs
+++ src/real_main.rs
@@ -5,9 +5,4 @@ use ab_os_bel::vga::WRITER;
 pub fn main() {
     WRITER.lock().clear();
     println!("Hello World{}", "!");
-    println!("Blink mode ? {}", ab_os_bel::vga::io_ports::is_blink_mode_enabled());
-    ab_os_bel::vga::io_ports::disable_blink_mode();
-    println!("Blink mode ? {}", ab_os_bel::vga::io_ports::is_blink_mode_enabled());
-    WRITER.lock().change_blink(true);
-    println!("Hello Blinky World{}", "!");
 }
