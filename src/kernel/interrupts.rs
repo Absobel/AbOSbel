59bb096e229bdf5309ce4f9820e4c5aa4961a058
Absobel <imkonnu@gmail.com>
2023-08-18 13:16:27 +0900

    start paging

--- src/kernel/interrupts.rs
+++ src/kernel/interrupts.rs
@@ -1,5 +1,5 @@
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 use crate::{gdt, print, println};
 
@@ -15,10 +15,12 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt.page_fault.set_handler_fn(page_fault_handler);
 
         // Hardware interruptions
 
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
@@ -50,15 +52,56 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     }
 }
 
-// TESTS
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
 
-#[cfg(test)]
-mod tests {
-    #[test_case]
-    fn no_panic_breakpoint_exception() {
-        x86_64::instructions::interrupts::int3();
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    crate::hlt_loop();
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: InterruptStackFrame)
+{
+    // TODO : Implement that myself but it shouldn't be necessary as in a later post it will be changed
+
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1,
+                HandleControl::Ignore)
+            );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
+
+
+
 
 // PIC8259
 use pic8259::ChainedPics;
@@ -77,6 +120,7 @@ pub static PICS: spin::Mutex<ChainedPics> =
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
@@ -88,3 +132,19 @@ impl InterruptIndex {
         usize::from(self.as_u8())
     }
 }
+
+
+
+
+
+// TESTS
+
+#[cfg(test)]
+mod tests {
+    #[test_case]
+    fn no_panic_breakpoint_exception() {
+        x86_64::instructions::interrupts::int3();
+    }
+}
+
+
--- src/real_main.rs
+++ src/real_main.rs
@@ -1,12 +1,14 @@
 use crate::println;
 use ab_os_bel::vga::WRITER;
+use x86_64::registers::control::Cr3;
 
 #[allow(dead_code)]
 pub fn main() {
     WRITER.lock().clear();
     println!("Hello World{}", "!");
 
-    WRITER.lock().clear();
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     println!("It did not crash!");
 }
