b5c5830229f2f8eb4f6fa069861760f413712d20
Absobel <imkonnu@gmail.com>
2023-08-19 19:29:54 +0900

    into long mode (wip)

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=linker.ld");
+    println!("cargo:rerun-if-changed=src/preliminary/boot.s");
+    println!("cargo:rerun-if-changed=src/preliminary/multiboot.s");
 }
--- start.sh
+++ start.sh
@@ -4,6 +4,13 @@ mkdir -p target/iso/boot/grub
 cp grub.cfg target/iso/boot/grub
 cp ${EXEC_PATH} target/iso/boot
 
-grub-mkrescue -o target/ab-os-bel.iso target/iso
+grub-mkrescue -o target/ab-os-bel.iso target/iso --verbose -d /usr/lib/grub/i386-pc
 
-qemu-system-x86_64 -cdrom target/ab-os-bel.iso
+# qemu-system-x86_64 -cdrom target/ab-os-bel.iso
+
+qemu-system-x86_64 \
+  -m "4G" \
+  -cdrom target/ab-os-bel.iso \
+  -monitor stdio \
+  -s -S
+  #-serial stdio \
