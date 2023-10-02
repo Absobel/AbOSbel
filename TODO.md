bd37314a2a8c9d1ff5ec5eee81e2637d39620afc
Absobel <imkonnu@gmail.com>
2023-10-02 05:25:10 +0200

    IJUSDHFFSEIUFES YES AGZGAGHAHAHAHHAHAHAHA je peux afficher des trucs et ça marche sur mon ordi omg

--- TODO.md
+++ TODO.md
@@ -1,13 +1,18 @@
 [x] - make it boot with grub
 [x] - return to the previous state (no matter the state of tests)
 [x] - make the test environnement work again
+[ ] - VGA to UEFI Framebuffer
+    [x] - Display smth
+    [ ] - Rewrite println! macro
+    [ ] - Resuse println! macro
 [ ] - restructure the project ?
 [ ] - continue the tutorial
-[ ] - VGA to UEFI Framebuffer
 [ ] - Logging
 [ ] - PIC to APIC
+[ ] - Write the multiboot part in rust, even the bootstrap if i'm feeling like it
 
 Future goals :
 
 [ ] - github actions ??
+[ ] - Global peace in the whole world
 [ ] - documentation
--- src/real_main.rs
+++ src/real_main.rs
@@ -1,3 +1,5 @@
+use core::arch::asm;
+
 use ab_os_bel::{
     memory::{self, FrameAllocator},
     vga::WRITER, serial_dbg,
@@ -7,42 +9,42 @@ use crate::println;
 
 #[allow(dead_code)]
 pub fn main() {
-    WRITER.lock().clear();
+    // WRITER.lock().clear();
 
-    println!(
-        "Total memory: {:.2} MiB",
-        memory::total_mem() as f64 / (1024.0 * 1024.0)
-    );
+    // println!(
+    //     "Total memory: {:.2} MiB",
+    //     memory::total_mem() as f64 / (1024.0 * 1024.0)
+    // );
 
-    println!("Areas :");
-    for area in unsafe {
-        memory::MULTIBOOT2_INFO
-            .as_ref()
-            .expect("Multiboot info required")
-            .memory_map_tag()
-            .expect("MemoryMapTag required")
-            .memory_areas()
-            .iter()
-            .filter(|area| area.typ() == multiboot2::MemoryAreaType::Available)
-    } {
-        println!(
-            "    Start: 0x{:x}, Length: {:.2} MiB",
-            area.start_address(),
-            area.size() as f64 / (1024.0 * 1024.0)
-        );
-    }
+    // println!("Areas :");
+    // for area in unsafe {
+    //     memory::MULTIBOOT2_INFO
+    //         .as_ref()
+    //         .expect("Multiboot info required")
+    //         .memory_map_tag()
+    //         .expect("MemoryMapTag required")
+    //         .memory_areas()
+    //         .iter()
+    //         .filter(|area| area.typ() == multiboot2::MemoryAreaType::Available)
+    // } {
+    //     println!(
+    //         "    Start: 0x{:x}, Length: {:.2} MiB",
+    //         area.start_address(),
+    //         area.size() as f64 / (1024.0 * 1024.0)
+    //     );
+    // }
 
-    let mut frame_allocator = memory::frame_allocator();
-    for i in 0.. {
-        if frame_allocator.allocate_frame().is_none() {
-            println!("Allocated {} frames", i);
-            println!(
-                "Or {:.2} MiB",
-                (i * memory::PAGE_SIZE) as f64 / (1024.0 * 1024.0)
-            );
-            break;
-        }
-    }
+    // let mut frame_allocator = memory::frame_allocator();
+    // for i in 0.. {
+    //     if frame_allocator.allocate_frame().is_none() {
+    //         println!("Allocated {} frames", i);
+    //         println!(
+    //             "Or {:.2} MiB",
+    //             (i * memory::PAGE_SIZE) as f64 / (1024.0 * 1024.0)
+    //         );
+    //         break;
+    //     }
+    // }
 
     serial_dbg!(unsafe {
         memory::MULTIBOOT2_INFO
@@ -54,15 +56,41 @@ pub fn main() {
     // serial_dbg!(unsafe {
     //     memory::MULTIBOOT2_INFO
     //         .as_ref()
-    //         .expect("Multiboot info required").efi_sdt64_tag();
-    // });
-
-    // serial_dbg!(unsafe {
-    //     memory::MULTIBOOT2_INFO
-    //         .as_ref()
     //         .expect("Multiboot info required")
     //         .efi_memory_map_tag()
     // });
 
-    println!("\nEnd of program.");
+
+    unsafe {
+        // Assuming the framebuffer_tag is the one you provided
+        let framebuffer_tag = memory::MULTIBOOT2_INFO.as_ref().expect("Multiboot info required").framebuffer_tag().expect("Framebuffer tag required").expect("Framebuffer tag required");
+        let framebuffer_addr = framebuffer_tag.address() as *mut u8;
+        let pitch = framebuffer_tag.pitch() as usize;
+        let width = framebuffer_tag.width() as usize;
+        let height = framebuffer_tag.height() as usize;
+        let bpp = framebuffer_tag.bpp() as usize / 8; // 3 bytes
+    
+        for y in 0..height {
+            for x in 0..width {
+                let offset = y * pitch + x * bpp;
+                let pixel_addr = framebuffer_addr.add(offset);
+    
+                if x < width / 2 {
+                    // Make it red: RGB(255, 0, 0)
+                    *pixel_addr.add(2) = 255; // R at position 16 (2 bytes offset)
+                    *pixel_addr.add(1) = 0;   // G at position 8  (1 byte offset)
+                    *pixel_addr = 0;          // B at position 0  (0 byte offset)
+                } else {
+                    // Make it purple: RGB(128, 0, 128)
+                    *pixel_addr.add(2) = 128; // R
+                    *pixel_addr.add(1) = 0;   // G
+                    *pixel_addr = 128;        // B
+                }
+            }
+        }
+    }
+        
+    
+
+    // println!("\nEnd of program.");
 }
